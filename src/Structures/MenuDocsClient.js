54649bb8c0e1a459303042b172e813e4fa45cf59
mandarin35 <68453402+mandarin35@users.noreply.github.com>
2020-10-23 01:24:37 +0600

    Update MenuDocsClient.js

--- src/Structures/MenuDocsClient.js
+++ src/Structures/MenuDocsClient.js
@@ -25,8 +25,8 @@ module.exports = class MenuDocsClient extends Client {
     validate(options) {
         if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
 
-        if (!options.token) throw new Error('You must pass the token for the client.');
-        this.token = options.token;
+        if (!options.process.env.TOKEN) throw new Error('You must pass the token for the client.');
+        this.process.env.TOKEN = options.process.env.TOKEN;
 
         if (!options.prefix) throw new Error('You must pass a prefix for the client.');
         if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
@@ -36,11 +36,11 @@ module.exports = class MenuDocsClient extends Client {
         this.defaultPerms = new Permissions(options.defaultPerms).freeze();
     }
 
-    async start(token = this.token) {
+    async start(token = this.process.env.TOKEN) {
         this.utils.loadCommands();
         this.utils.loadEvents();
 
-        await super.login(token);
+        await super.login(process.env.TOKEN);
     }
 
 };
